ac59bfda695056f434ccf5f1022bd18d914e48fc
Anna <anna.jagodzinska91@gmail.com>
2015-03-10 00:25:11 +0100

    added graph builder (first implementation)

---
+++ solver/src/main/scheduler/NotSoDummyNode.java
@@ -0,0 +1,36 @@
+package main.scheduler;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import main.productions.Production;
+
+public class NotSoDummyNode implements Node {
+	
+	
+	private Production production;
+	private List<Node> inNodes = new ArrayList<>();
+	private String name;
+	
+	public NotSoDummyNode(String name, Production prod){
+		this.name = name;
+		this.production = prod;
+	}
+
+	@Override
+	public Collection<Node> getInNodes() {
+		return inNodes;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	public void addInNode(Node node){
+		inNodes.add(node);
+	}
+
+
+}
---
+++ solver/src/main/scheduler/ProductionGraphBuilder.java
@@ -0,0 +1,65 @@
+package main.scheduler;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import main.productions.BackwardSubstitutionProduction;
+import main.productions.CombineChildrenProduction;
+import main.productions.PartialEliminationProduction;
+import main.productions.Production;
+import main.productions.SolveRootProduction;
+import main.tree.Vertex;
+
+public class ProductionGraphBuilder {
+
+	public Set<NotSoDummyNode> makeGraph(Vertex root) {
+		NotSoDummyNode rootNode = makeLeftSideOfGraph(root, null);
+		Set<NotSoDummyNode> leaves = new HashSet<>();
+		makeRightSideOfGraph(root, rootNode, leaves);
+		return leaves;
+
+	}
+
+	private NotSoDummyNode makeLeftSideOfGraph(Vertex root,
+			NotSoDummyNode parent) {
+
+		CombineChildrenProduction combineProd = new CombineChildrenProduction(
+				root);
+		NotSoDummyNode combineNode = new NotSoDummyNode("notroot", combineProd);
+		Production elimProd;
+
+		elimProd = parent != null ? new PartialEliminationProduction(root)
+				: new SolveRootProduction(root);
+		NotSoDummyNode elimNode = new NotSoDummyNode("notroot", elimProd);
+
+		if (parent != null) {
+			parent.addInNode(elimNode);
+		}
+		elimNode.addInNode(combineNode);
+
+		for (Vertex v : root.children) {
+			makeLeftSideOfGraph(v, combineNode);
+
+		}
+
+		return elimNode;
+
+	}
+
+	private void makeRightSideOfGraph(Vertex root, NotSoDummyNode parent,
+			Set<NotSoDummyNode> leaves) {
+
+		BackwardSubstitutionProduction bs = new BackwardSubstitutionProduction(
+				root);
+		NotSoDummyNode bsNode = new NotSoDummyNode("bs", bs);
+		bsNode.addInNode(parent);
+
+		if (root.children.isEmpty()) {
+			leaves.add(bsNode);
+		}
+		for (Vertex v : root.children) {
+			makeRightSideOfGraph(v, bsNode, leaves);
+		}
+	}
+
+}
